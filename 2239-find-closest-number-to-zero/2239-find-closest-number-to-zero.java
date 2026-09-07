b8e37199012e1abeb7a534d6cd5550f3dc793f9b
DEVANSH SINGH <devanshsingh21july@gmail.com>
2026-09-07 22:39:45 +0530

    Time: 8 ms (6.32%), Space: 47.2 MB (9.41%) - LeetHub

---
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == 0) {
+                return 0;
+            } else if (nums[mid] < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        if (left >= nums.length) return nums[right];
+        if (right < 0) return nums[left];
+        
+        if (Math.abs(nums[left]) <= Math.abs(nums[right])) {
+            return nums[left];
+        } else {
+            return nums[right];
+        }
+    }
+}
