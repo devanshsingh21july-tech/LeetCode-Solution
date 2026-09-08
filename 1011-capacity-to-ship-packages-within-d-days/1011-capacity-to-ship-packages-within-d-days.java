879318bce49d596c83125e3a5f9eb7a1c9372bd5
DEVANSH SINGH <devanshsingh21july@gmail.com>
2026-09-08 18:56:21 +0530

    Time: 11 ms (64.74%), Space: 50.2 MB (37.78%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,34 +1,42 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low=0;
-        int right=1;
-        for(int weight:weights){
-            low=Math.max(low,weight);
-            right+=weight;
+        int low = 0;
+        int right = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] > low) {
+                low = weights[i];
+            }
+            right += weights[i];
         }
-        int ans=right;
-        while(low<=right){
-            int mid=low+(right-low)/2;
-            if(newfunc(weights,days,mid)){
-                ans=mid;
-                right=mid-1;
-            }else{
-                low=mid+1;
+        
+        while (low <= right) {
+            int mid = low + (right - low) / 2;
+            
+            if (canShip(weights, days, mid)) {
+                right = mid - 1; 
+            } else {
+                low = mid + 1;  
             }
         }
-        return ans;
+        
+        return low; 
     }
-    private boolean newfunc(int[] weights,int days,int k){
-        int count=1;
-        int totalw=0;
-        for(int weight:weights){
-            if(totalw+weight>k){
+    
+    private boolean canShip(int[] weights, int days, int k) {
+        int count = 1;
+        int totalw = 0;
+        
+        for (int i = 0; i < weights.length; i++) {
+            if (totalw + weights[i] > k) {
                 count++;
-                totalw=weight;
-            }else{
-                totalw+=weight;
+                totalw = weights[i];
+                if (count > days) return false; 
+            } else {
+                totalw += weights[i];
             }
         }
-        return count<=days;
+        
+        return true;
     }
 }
