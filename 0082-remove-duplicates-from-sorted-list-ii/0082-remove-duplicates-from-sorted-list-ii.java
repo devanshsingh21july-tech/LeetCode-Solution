f8c1682d40dc9df05834ee004dbec290c98251c1
DEVANSH SINGH <devanshsingh21july@gmail.com>
2026-09-17 15:38:47 +0530

    Time: 0 ms (100%), Space: 44.9 MB (55.5%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,8 +10,9 @@ class Solution {
             if(head.next!=null&&head.val==head.next.val){
                 while(head.next!=null&&head.val==head.next.val){
                     head=head.next;
+                    prev.next=head.next;
                 }
-                prev.next=head.next;
+                
             }else{
                 prev=prev.next;
             }
