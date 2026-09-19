6356a3c25b1d9915b90a24eb212f909674a810ad
DEVANSH SINGH <devanshsingh21july@gmail.com>
2026-09-19 16:14:55 +0530

    Time: 4 ms (100%), Space: 225 MB (95.52%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -1,27 +1,18 @@
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        // Nayi list ke liye Dummy head aur Tail pointer
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-
-        // First 0 ko skip karke Node 3 se start karo
-        ListNode curr = head.next; 
-        int sum = 0;
-
-        while (curr != null) {
-            if (curr.val != 0) {
-                // Jab tak zero nahi aata, sum mein add karte raho
-                sum += curr.val;
-            } else {
-                // Zero aane par naya node banao sum waali value ka
-                tail.next = new ListNode(sum);
-                tail = tail.next; // Tail ko naye node par shift karo
-                
-                sum = 0; // Agle segment ke liye sum reset karo
-            }
-            curr = curr.next;
+        ListNode modify=head.next;
+        ListNode nextadd=modify;
+        while(nextadd!=null){
+            int sum=0;
+            while(nextadd.val!=0){
+                sum+=nextadd.val;
+                nextadd=nextadd.next;
+            }modify.val=sum;
+            nextadd=nextadd.next;
+            
+            modify.next=nextadd;
+            modify=modify.next;
         }
-
-        return dummy.next; // Nayi list ka actual head return karo
+        return head.next;
     }
 }
